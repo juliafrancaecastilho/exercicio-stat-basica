a4da4c9c4b079cf4ce1b302de09f9c27edb6f135
Julia Franca e Castilho <juliafrancaecastilho@gmail.com>
2026-03-25 20:38:25 -0300

    Melhora o gráfico de pizza (com ggplot2)

--- analise.R
+++ analise.R
@@ -14,5 +14,18 @@ ggplot(dados, aes(x = nomes, y = idades)) +
   theme_minimal() +
   labs(title = "Gráfico de Barras de Idades", x = "Nomes", y =  "Idades")
 
-pie(c(22, 56, 61, 20), labels = c("Júlia", "Melanie", "André", "Carol"), col = c("#66c2a5", "#fc8d62", "#8da0cb", "#e78ac3"),
-    main = "Gráfico de Pizza - Idades")
+ggplot(dados, aes(x = "", y = idades, fill = nomes)) +
+  geom_bar(stat = "identity", width = 1, color = "white") +
+  coord_polar("y") +
+  geom_text(aes(label = idades),
+            position = position_stack(vjust = 0.5),
+            color = "white",
+            size = 5) +
+  labs(title = "Gráfico de Pizza - Idades em anos") +
+  theme_void() +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 16),
+    legend.title = element_blank()
+  )
+
+  
