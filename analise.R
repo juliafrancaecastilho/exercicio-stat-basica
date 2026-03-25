b432fc32721136bbf91fdfdb0f4892c8b32e6897
Julia Franca e Castilho <juliafrancaecastilho@gmail.com>
2026-03-25 18:32:14 -0300

    Adiciona gráfico de pizza

--- analise.R
+++ analise.R
@@ -13,3 +13,6 @@ ggplot(dados, aes(x = nomes, y = idades)) +
   geom_bar(stat = "identity", fill = "skyblue") +
   theme_minimal() +
   labs(title = "Gráfico de Barras de Idades", x = "Nomes", y =  "Idades")
+
+pie(c(22, 56, 61, 20), labels = c("Júlia", "Melanie", "André", "Carol"), col = c("#66c2a5", "#fc8d62", "#8da0cb", "#e78ac3"),
+    main = "Gráfico de Pizza - Idades")
